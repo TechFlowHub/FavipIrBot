71fe6d622cd847af7f5b992f0ffc311e382d7618
viniimiguel <viniciusmiguelgit2@gmail.com>
2025-03-10 11:26:51 -0300

    solving-merge

--- Bot.py
+++ Bot.py
@@ -115,6 +115,7 @@ class Bot:
 
             # Decodifica o QR Code
             qr_code = pyzbar.decode(qr_np)
+            print(qr_code)
           
             if qr_code:
                 qr_text = qr_code[0].data.decode("utf-8")
